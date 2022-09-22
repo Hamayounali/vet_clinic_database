7abd60f671eb322237322ee9f249d2c7f9b01188
Hamayoun <hamayounemail@gmail.com>
2022-09-22 13:59:15 +0430

    Query multiple tables
    1. Created multiple tables
    2. Used primary key & foreign key mechanism for joining tables.
    3. Prepared complex queries that answer analytical questions.

--- schema.sql
+++ schema.sql
@@ -4,11 +4,23 @@ CREATE DATABASE vet_clinic;
 alter table animals add species varchar(100);
 
 CREATE TABLE animals (
-ID int,
+ID  SERIAL PRIMARY KEY,
 name varchar(100),
 date_of_birth date,
 escape_attempts int,
 neutered boolean,
 weight_kg decimal(10,2),
-species varchar(100)
+species_id int,
+owner_id int
+);
+
+create table owners (
+ID  SERIAL PRIMARY KEY,
+full_name varchar(100),
+age int
+);
+
+create table species (
+ID  SERIAL PRIMARY KEY,
+name varchar(100)
 );
